e11c26073a3ba397211ec31013f0bbe18b998c65
Chris Kuchin <github@kchn.dev>
2023-08-13 13:15:06 -0600

    firebase app

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXgirQia_YKKuK_tHOK1ZLgK1W9qKWp_c",
+  authDomain: "rslts-run.firebaseapp.com",
+  projectId: "rslts-run",
+  storageBucket: "rslts-run.appspot.com",
+  messagingSenderId: "601913404882",
+  appId: "1:601913404882:web:231b67ef157fac9888ac0b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
