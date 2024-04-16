6d7699ffc4f4885bd16638e437221b7ed680e6ef
Chris Kuchin <chriskuchin@gmail.com>
2024-04-15 22:27:58 -0600

    update the interface definition

--- pkg/db/interface.go
+++ pkg/db/interface.go
@@ -8,4 +8,8 @@ import (
 type DB interface {
 	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 	SelectContext(ctx context.Context, destination interface{}, query string, args ...interface{})
+	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
+	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
+	GetContext(ctx context.Context, destination interface{}, query string, args ...interface{}) error
+	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
 }
